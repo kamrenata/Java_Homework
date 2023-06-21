858a4531b4d07b37004740a9b06fa1386a161af8
Renata Kamalova <renatakamal@gmail.com>
2023-06-21 20:39:13 +0400

    added GitHub Test

---
+++ src/test/java/kam/renata/githubAutotest.java
@@ -0,0 +1,29 @@
+package kam.renata;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+
+public class githubAutotest extends TestBase {
+
+    @Test
+    void JUnit5Check(){
+        //Открыть страницу Selenide в Github
+        open("/");
+        $("[placeholder='Search GitHub']").setValue("Selenide").pressEnter();
+        $(byText("selenide/")).click();
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+        //Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        //Открыть страницу SoftAssertions, проверить, что внутри есть пример кода для JUnit5
+        $(".markdown-body").$(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+
+}
